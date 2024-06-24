a92b3de7d41251cf67e0b05e478d53620f067cf7
qkenn <github.com.sampling990@passinbox.com>
2024-06-24 12:39:18 +0530

    middleware setup

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const path = require('path');
-const main = require('./routes/main');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 
+// environment variables
 const port = process.env.PORT;
 
+// initializing
 const app = express();
+
+// importing routes
+const main = require('./routes/main');
+
+// middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // setting view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// routes
 app.use('/', main);
 
+// server
 app.listen(port);
