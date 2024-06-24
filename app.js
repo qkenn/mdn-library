d4880fd774fddad42589c1127c3ea07d9b57050d
qkenn <github.com.sampling990@passinbox.com>
2024-06-24 12:34:04 +0530

    pug setup

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const main = require('./routes/main');
 
 const port = process.env.PORT;
 
 const app = express();
+
+// setting view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use('/', main);
 
