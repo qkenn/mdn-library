ae1cc0c2df17cb1ff749ea5b0930a98795ac559d
qkenn <github.com.sampling990@passinbox.com>
2024-06-25 13:39:54 +0530

    Add mongoose and connect to MongoDB

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const index = require('./routes/main');
 
 // environment variables
 const port = process.env.PORT;
+const mongoDB = process.env.MONGODB_URI;
 
 // initializing
 const app = express();
 
-// importing routes
-const main = require('./routes/main');
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log('atlas connected');
+}
 
 // middleware
 app.use(logger('dev'));
@@ -26,7 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // routes
-app.use('/', main);
+app.use('/', index);
 
 // server
 app.listen(port);
